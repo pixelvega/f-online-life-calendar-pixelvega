12c212a844a12f15b6f6c174b7e5388b16980caa
pixelvega <pixelvega.info@gmail.com>
2019-03-25 03:50:35 +0100

    docs

---
+++ docs/precache-manifest.39f402ae77e680d50ddd888ac97f6d1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9ba6f30b0ae8d4789a09",
+    "url": "./static/js/main.3358d4c5.chunk.js"
+  },
+  {
+    "revision": "923d324f81c98e79de5a",
+    "url": "./static/js/2.c9912e99.chunk.js"
+  },
+  {
+    "revision": "9ba6f30b0ae8d4789a09",
+    "url": "./static/css/main.52a2fbf0.chunk.css"
+  },
+  {
+    "revision": "396e2bd3ce0f6df19df65c035751935d",
+    "url": "./index.html"
+  }
+];
